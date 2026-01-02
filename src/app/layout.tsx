e3ae188ddae8b1ddeb5f0647858459670e001b46
Anisul Kibria <du.shaheen@gmail.com>
2026-01-02 10:46:16 +0600

    gtag added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,19 +37,28 @@ export default function RootLayout({
 
   return (
     <html lang="en" className={`${inter.variable} antialiased`} suppressHydrationWarning>
-      <body className="font-sans antialiased text-slate-800 bg-white" suppressHydrationWarning>
+      <head>
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-B4NFMJ7ZXD"
-          strategy="afterInteractive"
+          id="google-tag-manager"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5S996QH3');`
+          }}
         />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-B4NFMJ7ZXD');
-          `}
-        </Script>
+      </head>
+      <body className="font-sans antialiased text-slate-800 bg-white" suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5S996QH3"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <FontLoader />
         <Header />
         <main>{children}</main>
